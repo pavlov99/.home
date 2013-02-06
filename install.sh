a22525bd9830b90b835957c71f3cb032d6d412ec
Kirill Pavlov <kirill.pavlov@phystech.edu>
2013-02-06 15:04:29 +0000

    move gitconfig to .gitconfig

--- install.sh
+++ install.sh
@@ -1,14 +1,5 @@
 apt-get install curl bash git-core mercurial
 
-# Git settings
-git config --global user.email "kirill.pavlov@phystech.edu"
-git config --global user.name "Kirill Pavlov"
-
-git config --global color.branch auto
-git config --global color.diff auto
-git config --global color.interactive auto
-git config --global color.status auto
-
 hg clone https://vim.googlecode.com/hg/ vim
 
 # Install rvm and stable version of ruby for vim plugins
