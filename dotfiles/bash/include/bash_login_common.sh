d440ccfa4b474894f1dd5965184f847ad8f0563f
Kirill Pavlov <kirill.pavlov@phystech.edu>
2013-03-23 22:02:04 +0000

    add macos info commands

--- dotfiles/bash/include/bash_login_common.sh
+++ dotfiles/bash/include/bash_login_common.sh
@@ -13,17 +13,31 @@ function get_system_core_name_macos {
 function get_free_memory_info_ubuntu {
     free -m | grep "Mem:" | awk '{print "Used: "$3"Mb/"$2"Mb ("int($3/$2*100)"%) Free: "$4"Mb"}'
 }
-get_free_disk_space_info
-get_system_core_name_ubuntu
 
+function get_free_memory_info_mac {
+    # total_memory=$(ps -caxm -orss,comm | awk 'BEGIN{mem=0}{mem+=$1}END{print mem/1024}')
+    total_memory=$(sysctl hw.memsize | awk '{print $2/(1024^2)}')
+    # free_memory=$(vm_stat | grep "Pages free:" | awk '{print $3 * 4096 / (1024^2)}')
+    free_memory=$(vm_stat | grep "Pages free:" | awk '{print $3 * 4 / 1024}')
+    echo | awk -v fm=$free_memory -v tm=$total_memory '{print "Used: "tm-fm"Mb/"tm"Mb ("int((tm-fm)/tm*100)"%) Free: "fm"Mb"}'
+}
+
+
+function get_ip_address_ubuntu {
+    ip addr show eth0 | grep "inet " | awk '{print $2}'
+}
+get_free_disk_space_info
+# get_system_core_name_ubuntu
 
 echo -e "Date:\t\t"$(date)
 echo -e "User:\t\t"$(whoami)"@"$(hostname)
 echo -e "Login users:\t"$(users)
 echo -e "Current path:\t"$(pwd)
 echo -e "Uptime:\t\t"$(uptime)
-echo -e "Server IP:\t"$(ifconfig | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}')
-# ip addr show eth0 | grep "inet " | awk '{print $2}'
+echo "Bash version: "${BASH_VERSION%.*}
 
-echo "Bash version"${BASH_VERSION%.*}
-cat /proc/cpuinfo | grep "model name" | cut -d:  -f2 | cut -d" " -f2- | uniq -c # ubuntu
+# cat /proc/cpuinfo | grep "model name" | cut -d:  -f2 | cut -d" " -f2- | uniq -c # ubuntu
+# sudo lshw # ubuntu show hardware info
+# vmstat # also memory stat ubuntu
+# system_profiler # mac hardware overview
+# sysctl -a # mac low level overview
