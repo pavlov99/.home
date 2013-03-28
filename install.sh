48b97904697ff2bcc876ade7e1220c6b5eb46f11
Kirill Pavlov <kirill.pavlov@phystech.edu>
2013-03-28 15:34:28 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 (
-  [ -d $HOME/.home ] &&
-  echo "remove your "$HOME"/.home directory" &&
-  exit 1
-) || ( 
+  [ ! -d $HOME/.home ] &&
   git clone git@github.com:pavlov99/.home.git &&
   cd $HOME/.home &&
   make && make install
+) || ( 
+  echo "remove your "$HOME"/.home directory" && exit 1
 )
