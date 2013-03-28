596f7246a4707cf0986bbde2484250592d36e027
Kirill Pavlov <kirill.pavlov@phystech.edu>
2013-03-28 15:11:21 +0000

    update install script

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
-# apt-get install curl bash git-core mercurial
-
-# hg clone https://vim.googlecode.com/hg/ vim
-
-# Install rvm and stable version of ruby for vim plugins
-# curl -L https://get.rvm.io | bash -s stable --ruby
-ls
+(
+  [ -d $HOME/.home ] &&
+  echo "remove your "$HOME"/.home directory first"
+) || ( 
+  git clone https://github.com/pavlov99/.home.git
+)
