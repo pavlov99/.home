36cc473028f47437ca21f4630a4dcb04d1b773fe
Kirill Pavlov <kirill.pavlov@phystech.edu>
2013-03-28 14:56:22 +0000

    try to use install script

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
-apt-get install curl bash git-core mercurial
+# apt-get install curl bash git-core mercurial
 
-hg clone https://vim.googlecode.com/hg/ vim
+# hg clone https://vim.googlecode.com/hg/ vim
 
 # Install rvm and stable version of ruby for vim plugins
-curl -L https://get.rvm.io | bash -s stable --ruby
+# curl -L https://get.rvm.io | bash -s stable --ruby
+ls
